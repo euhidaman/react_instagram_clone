baacf72426e42e19c2088ef463d609b41b8cb906
Euhid Aman <aman.derax20@gmail.com>
2021-06-13 20:51:41 +0530

    Created a simple modal using material-ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { db } from './firebase';
 import Post from "./Post";
-//57:30
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+
+function getModalStyle() {
+  const top = 100;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 250,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function App() {
 
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     db.collection('posts').onSnapshot(
@@ -17,14 +47,37 @@ function App() {
     )
   }, []);
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
+
     <div className="app">
+
+      <Modal open={open} onClose={handleClose} >
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+            />
+          </center>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
       </div>
+
+      <Button onClick={handleOpen}>Sign Up</Button>
 
       {
         posts.map(({id, post}) => (
